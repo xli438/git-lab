1198313eae794dea7e8101da862865bdcbc3db68
Xiao Li <xli438@usc.edu>
2013-11-14 19:10:06 -0800

    test for branch

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,13 +1,13 @@
 public class FizzBuzz {
     public static void main(String[] args) {
         for(int i = 0; i < 100; i++) {
-//<<<<<<< HEAD
+
             if( i % 3 == 0)
             System.out.println("Fizz");
-//=======
+
             else if( i % 5 == 0)
                 System.out.println("Buzz");
-//>>>>>>> bfb5368fdbe09f881a45fb89ade87e9dd6b6d7d6
+
         }
     }
 }
